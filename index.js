48c57ebdb50c9b4cf80de813b444a37ab1a9fb6e
Ketul8621 <77052683+Ketul8621@users.noreply.github.com>
2021-12-12 13:08:35 +0530

    Everything in one commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const chatIcon = document.querySelector("#chat-icon");
+const iframe = document.querySelector("iframe");
+const close = document.querySelector("#close");
+
+console.log(iframe);
+chatIcon.addEventListener("click", () => {
+  console.log(iframe);
+  iframe.src = "/chat-container.html";
+  chatIcon.style.display = "none";
+  close.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  console.log("clicked");
+  iframe.src = "";
+  chatIcon.style.display = "block";
+  close.style.display = "none";
+});
